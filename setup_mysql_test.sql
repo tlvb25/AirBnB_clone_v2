d64a55e6bb48fed2d996fddd7e4442108a844474
T <test@mail.com>
2019-08-12 23:30:13 -0700

    code chg

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,8 @@
+-- create A database hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+-- create user hbnb_dev ans set password
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+-- grant all privileges for hbnb_dev
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+-- grant select privileges
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
