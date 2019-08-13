974f72c637e99c93c6f2573d141bcb931dd36b48
T <test@mail.com>
2019-08-12 22:46:22 -0700

    code chg

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,8 @@
+-- create A database hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-USE hbnb_dev_db;
+-- create user hbnb_dev ans set password
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-USE performance_schema;
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+-- grant all privileges for hbnb_dev
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+-- grant select privileges
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
