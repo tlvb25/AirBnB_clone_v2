b3ff888e688fce0518ae732240508e1ee7a2c5fc
T <test@mail.com>
2019-08-12 18:38:23 -0700

    script for creating DB and creating user

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+USE hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'user_0d_1'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'user_0d_1'@'localhost';
