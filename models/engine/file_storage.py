1ee24ddade5b4bfb10235bd934a029ca9a4dd6da
T <test@mail.com>
2019-08-13 17:24:47 -0700

    code chg

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -69,5 +69,6 @@ class FileStorage:
         if obj is None:
             return
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        del self.__objects[key]
+        if key in self.__objects:
+            del self.__objects[key]
         self.save()
