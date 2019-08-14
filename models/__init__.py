65a6add90c7b690c24b1e8c4fbe17fedaaa10ea3
T <test@mail.com>
2019-08-14 02:09:19 -0700

    code chg

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """create a unique FileStorage instance according DB used"""
-from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -10,11 +9,11 @@ from models.place import Place
 from models.review import Review
 from os import getenv
 
-
 if getenv('HBNB_TYPE_STORAGE') == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
     storage.reload()
 else:
+    from models.engine.file_storage import FileStorage
     storage = FileStorage()
     storage.reload()
