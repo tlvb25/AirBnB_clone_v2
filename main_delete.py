85431fc8cf0012874cca3659971a4b9dd947e304
T <test@mail.com>
2019-08-13 17:18:23 -0700

    code corr

--- main_delete.py
+++ main_delete.py
@@ -12,24 +12,24 @@ print("All States: {}".format(len(all_states.keys())))
 for state_key in all_states.keys():
     print(all_states[state_key])
 
-# # Create a new State
-# new_state = State()
-# new_state.name = "California"
-# fs.new(new_state)
-# fs.save()
-# print("New State: {}".format(new_state))
+# Create a new State
+new_state = State()
+new_state.name = "California"
+fs.new(new_state)
+fs.save()
+print("New State: {}".format(new_state))
 
-# # All States
-# all_states = fs.all(State)
-# print("All States: {}".format(len(all_states.keys())))
-# for state_key in all_states.keys():
-#     print(all_states[state_key])
+# All States
+all_states = fs.all(State)
+print("All States: {}".format(len(all_states.keys())))
+for state_key in all_states.keys():
+    print(all_states[state_key])
 
 # Delete the new State
 fs.delete(new_state)
 
-# # All States
-# all_states = fs.all(State)
-# print("All States: {}".format(len(all_states.keys())))
-# for state_key in all_states.keys():
-#     print(all_states[state_key])
+# All States
+all_states = fs.all(State)
+print("All States: {}".format(len(all_states.keys())))
+for state_key in all_states.keys():
+    print(all_states[state_key])
