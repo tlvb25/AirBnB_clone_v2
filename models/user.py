abc9c4782361287e8aa11b18dea18abd6ac5f6b2
Cyn <cmdominguez@gmail.com>
2019-08-14 21:03:57 +0000

    Additions of class attributes to User

--- models/user.py
+++ models/user.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
 """This is the user class"""
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
+import sqlalchemy
+import sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
+from os import getenv
 
-
-class User(BaseModel):
+class User(BaseModel, Base):
     """This is the class for user
     Attributes:
         email: email address
-        password: password for you login
+        password: password for your login
         first_name: first name
         last_name: last name
     """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    # DBStorage class attribute
+    __tablename__ = 'users'
+    # DBStorage class attribute
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
