17027fdd21ba9ae3a63d64a9a118a7ef1785c3cf
tlvb25 <638@holbertonschool.com>
2019-08-14 09:21:32 +0000

    fixed indent

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -22,7 +22,7 @@ class DBStorage:
                                              getenv('HBNB_MYSQL_PWD'),
                                              getenv('HBNB_MYSQL_HOST'),
                                              getenv('HBNB_MYSQL_DB')),
-                                        pool_pre_ping=True)
+                                      pool_pre_ping=True)
 
         if getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(bind=self.__engine)
