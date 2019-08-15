50f60a08285656114194ab696979545da360da87
T <test@mail.com>
2019-08-15 01:10:22 -0700

    pep8

--- models/amenity.py
+++ models/amenity.py
@@ -1,22 +1,9 @@
 #!/usr/bin/python3
 """This is the amenity class"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Table, ForeignKey
-from sqlalchemy.orm import relationship
 from os import getenv
-
-if getenv("HBNB_TYPE_STORAGE") == "db":
-    place_amenity = Table("place_amenity", Base.metadata,
-                          Column("place_id",
-                                 String(60),
-                                 ForeignKey("places.id"),
-                                 primary_key=True,
-                                 nullable=False),
-                          Column("amenity_id",
-                                 String(60),
-                                 ForeignKey("amenities.id"),
-                                 primary_key=True,
-                                 nullable=False))
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
 
 
 class Amenity(BaseModel, Base):
@@ -24,13 +11,9 @@ class Amenity(BaseModel, Base):
     Attributes:
         name: input name
     """
-    __tablename__ = "amenities"
-    name = Column(String(128),
-                  nullable=False)
-
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        place_amenities = relationship(
-            "Place",
-            secondary=place_amenity,
-            viewonly=False,
-            back_populates="amenities")
+        place_amenities = relationship('Place',
+                                       secondary='place_amenity',
+                                       back_populates='amenities')
