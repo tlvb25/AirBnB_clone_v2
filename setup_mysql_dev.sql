ed7acf5c6f120a832667e48ea2066a7096b8bef3
T <test@mail.com>
2019-08-14 22:12:31 -0700

    sql script to create DB and User

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,10 @@
 -- create A database hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- create user hbnb_dev ans set password
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
+ALTER USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- grant all privileges for hbnb_dev
+GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 -- grant select privileges
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
