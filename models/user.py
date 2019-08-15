b7337c175d10bcdfde5cfb114f66f3d26f70b63a
T <test@mail.com>
2019-08-14 18:00:49 -0700

    code chg

--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This is the user class"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import String, Column
 
 
 class User(BaseModel, Base):
@@ -15,9 +15,6 @@ class User(BaseModel, Base):
     __table__ = 'users'
 
     email = Column(String(128), nullable=False)
-
     password = Column(String(128), nullable=False)
-
     first_name = Column(String(128), nullable=True)
-
     last_name = Column(String(128), nullable=True)
