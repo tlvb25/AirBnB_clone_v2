84e37dd63e33d14c16ac2e911168d45899f07895
T <test@mail.com>
2019-08-14 23:41:45 -0700

    updated file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,75 +1,70 @@
 #!/usr/bin/python3
-"""This is the file storage class for AirBnB"""
-import json
-import datetime
+"""Serializes instances to a JSON file and deserializes
+JSON file to instances
+"""
 from models.base_model import BaseModel
 from models.user import User
-from models.state import State
-from models.city import City
 from models.amenity import Amenity
+from models.city import City
 from models.place import Place
 from models.review import Review
+from models.state import State
+import json
+from os import path
+import datetime
 
 
 class FileStorage:
-    """This class serializes instances to a JSON file and
-    deserializes JSON file to instances
-    Attributes:
-        __file_path: path to the JSON file
-        __objects: objects will be stored
-    """
-    __file_path = "file.json"
+    """Class to hold information and saved instances"""
+
+    __file_path = 'file.json'
     __objects = {}
 
-    def all(self, cls=None):
-        """returns a dictionary
-        Return:
-            returns a dictionary of __object
-        """
-        if cls is None:
-            return self.__objects
-        else:
-            my_dict = {}
-            for key, value in self.__objects.items():
-                if key.split(".")[0] == cls.__name__:
-                    my_dict[key] = value
-            return my_dict
+    def all(self):
+        """Returns all objects stored"""
+
+        return self.__objects
 
     def new(self, obj):
-        """sets __object to given obj
+        """Formats the objects to be set in self.__objects
         Args:
-            obj: given object
+           obj - the class type
         """
-        if obj:
-            key = "{}.{}".format(type(obj).__name__, obj.id)
-            self.__objects[key] = obj
+
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
 
     def save(self):
-        """serialize the file path to JSON file path
-        """
-        my_dict = {}
-        for key, value in self.__objects.items():
-            my_dict[key] = value.to_dict()
-        with open(self.__file_path, 'w', encoding="UTF-8") as f:
-            json.dump(my_dict, f)
+        """Serializes __objects to the JSON file (path: __file_path)"""
+
+        tmp_dict = {}
+        for k, v in self.__objects.items():
+            tmp_dict[k] = v.to_dict()
+        with open(self.__file_path, "w", encoding="utf-8") as written_file:
+            json.dump(tmp_dict, written_file)
 
     def reload(self):
-        """serialize the file path to JSON file path
+        """Deserializes the JSON file to __objects (only if the JSON file
+        (__file_path))
         """
-        try:
-            with open(self.__file_path, 'r', encoding="UTF-8") as f:
-                for key, value in (json.load(f)).items():
-                    value = eval(value["__class__"])(**value)
-                    self.__objects[key] = value
-        except FileNotFoundError:
-            pass
 
-    def delete(self, obj=None):
-        """to delete obj from __objects if it’s inside
-        """
-        if obj is None:
-            return
-        key = "{}.{}".format(type(obj).__name__, obj.id)
-        if key in self.__objects:
-            del self.__objects[key]
-        self.save()
+        dict_of_dicts = {}
+        classes = {
+            "BaseModel": BaseModel,
+            "User": User,
+            "Amenity": Amenity,
+            "City": City,
+            "Place": Place,
+            "Review": Review,
+            "State": State}
+
+        try:
+            temp_dict = {}
+            with open(self.__file_path, "r") as r:
+                dict_of_dicts = json.load(r)
+            for k, v in dict_of_dicts.items():
+                if v['__class__'] in classes:
+                    temp_dict[k] = classes[v['__class__']](**v)
+            self.__objects = temp_dict
+        except Exception:
+            pass
