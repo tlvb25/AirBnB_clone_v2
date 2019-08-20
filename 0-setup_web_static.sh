0ff0a0261f2a5094f82545043f9531e251ef6010
T <test@mail.com>
2019-08-19 23:14:48 -0700

    script changes

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-# script that sets up your web servers for the deployment of web_static
+# script that configures both my web servers
 apt-get update
-dpkg -l | grep -qw nginx || apt-get install nginx
+dpkg -l | grep -qw nginx || apt-get -y install nginx
 ufw allow 'Nginx HTTP'
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
@@ -11,9 +11,9 @@ echo "<html>
   <body>
     Holberton School
   </body>
-</html>t" > /data/web_static/releases/test/index.html
+</html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+chown -R ubuntu:ubuntu /data/
+sed -i '/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 service nginx restart
 exit 0
