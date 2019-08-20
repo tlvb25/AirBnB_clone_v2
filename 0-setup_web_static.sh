fbb03e781018dfc67fab6e6d90b70b45ec27a8e0
T <test@mail.com>
2019-08-19 21:29:10 -0700

    scripting for task 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# script that sets up your web servers for the deployment of web_static
+apt-get update
+dpkg -l | grep -qw nginx || apt-get install nginx
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo "this is just a test" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+service nginx restart
+exit 0
