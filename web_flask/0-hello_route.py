029a057530830f24ff6d2ee4c05bf20f9c14efda
T <test@mail.com>
2019-09-04 18:33:17 -0700

    script change

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,10 +6,11 @@ from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
 def hello_betty():
     """function return string using flask"""
-    return “Hello HBNB!”
+    return 'Hello HBNB!'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
