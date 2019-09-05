c030c7e53b36575e9436a3b580a262db3cad272a
T <test@mail.com>
2019-09-04 18:52:46 -0700

    corr

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-"""1. HBNB """
+"""1. HBNB"""
 from flask import Flask
 
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
 def hello_betty():
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb')
 def hello_hbnb():
