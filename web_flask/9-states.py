5b597f1652ba1ce2ce915b330aaabe73f96a1769
T <test@mail.com>
2019-09-05 16:30:06 -0700

    code change

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -21,7 +21,7 @@ def show_OnlyStates():
 
 
 @app.route('/states/<id>')
-def show_cities_of_state(id=None):
+def show_cities_of_state(id):
     records = storage.all('State')
     return render_template('9-states.html', id=id, records=records)
 
