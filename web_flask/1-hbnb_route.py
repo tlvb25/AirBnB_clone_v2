519b751891204805da159cf00cef7ad35e18c05f
T <test@mail.com>
2019-09-04 18:44:38 -0700

    script for task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""1. HBNB """
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route('/')
+def hello_betty():
+    return 'Hello HBNB'
+
+@app.route('/hbnb')
+def hello_hbnb():
+    return 'HBNB'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
