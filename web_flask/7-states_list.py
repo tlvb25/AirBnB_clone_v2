d2adef0731ab0847174608e2eacc4d1d3b452742
T <test@mail.com>
2019-09-08 21:27:29 -0700

    code correction

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -48,7 +48,7 @@ def oddEven_template(n):
 
 @app.route('/states_list')
 def run_all_states():
-    records = storage.all('State')
+    records = storage.all("State").values()
     return render_template('7-states_list.html', records=records)
 
 
