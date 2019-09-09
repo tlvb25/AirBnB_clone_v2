b5756abde8f6b9fce011d3a674657371e4279539
T <test@mail.com>
2019-09-08 22:21:51 -0700

    code corrections

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -11,13 +11,15 @@ app.url_map.strict_slashes = False
 
 @app.route('/hbnb_filters')
 def hbnb_filters():
-    """Filters Section"""
-    stateRec = storage.all('State')
-    amenityRec = storage.all('Amenity')
-    return render_template('10-hbnb_filters.html', states=stateRec, amenities=amenityRec)
+    states = storage.all('State')
+    amenities = storage.all('Amenity')
+    return render_template('10-hbnb_filters.html', states=states, amenities=amenities)
 
 
 @app.teardown_appcontext
 def do_teardown(self):
     """Closes session"""
     storage.close()
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port='5000')
