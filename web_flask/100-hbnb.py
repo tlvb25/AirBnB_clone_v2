f2180ae5bf239aa742801b7fa28305062c9f6138
T <test@mail.com>
2019-09-08 22:31:53 -0700

    code corrections

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
 """12. HBNB is alive!"""
-from flask import Flask, abort, render_template
+from flask import Flask, escape, render_template
 from models import storage
 
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
 
 
 @app.route('/hbnb')
-def show_cityState():
-    stateRec = storage.all('State')
-    amenRec = storage.all('Amenity')
-    return render_template('100-hbnb.html', states=stateRec, amenities=amenRec)
+def hbnb():
+    """ A route that displays the main hbnb page """
+    states = storage.all('State')
+    amenities = storage.all('Amenity')
+    places = storage.all('Place')
+    users = storage.all('User')
+    return render_template('100-hbnb.html',
+                           states=states, amenities=amenities,
+                           places=places, users=users)
 
 
 @app.teardown_appcontext
